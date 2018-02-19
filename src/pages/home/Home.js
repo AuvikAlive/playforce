6c8b77a85bdec902585880a4ca62b7a22a259dea
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-02-19 13:24:01 +0600

    added Material-UI

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import Button from 'material-ui/Button'
+import AccessAlarmIcon from 'material-ui-icons/AccessAlarm'
 
-const Home = () => <div>Home</div>
+const Home = () => (
+  <div>
+    <Button variant="raised" color="primary">
+      Hello World
+    </Button>
+    <AccessAlarmIcon />
+  </div>
+)
 
 export default Home
