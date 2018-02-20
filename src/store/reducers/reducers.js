f70f26481d677c6b9a736b8bf6370c340da85bd5
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-02-20 14:50:13 +0600

    added react-redux-firebase

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import { firebaseReducer } from 'react-redux-firebase'
 
 export const reducers = {
-  router: routerReducer
+  router: routerReducer,
+  firebase: firebaseReducer
 }
 
 export const rootReducer = combineReducers(reducers)
--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,37 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
 import thunk from 'redux-thunk'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/es/storage'
+import firebase from 'firebase'
+import { reactReduxFirebase } from 'react-redux-firebase'
 import { rootReducer } from './reducers/reducers'
-import createHistory from 'history/createBrowserHistory'
+import { firebaseConfig } from '../config/firebase'
 
-const config = {
-  key: 'root',
-  storage
-}
-
-const persistedReducer = persistReducer(config, rootReducer)
+const persistedReducer = persistReducer(
+  {
+    key: 'root',
+    storage
+  },
+  rootReducer
+)
 
 export const history = createHistory()
 const router = routerMiddleware(history)
+
+firebase.initializeApp(firebaseConfig)
+const firebaseStore = reactReduxFirebase(firebase, {
+  userProfile: 'users'
+})
 
 const middleware = applyMiddleware(thunk, router)
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export let store = createStore(persistedReducer, composeEnhancers(middleware))
+export let store = createStore(
+  persistedReducer,
+  composeEnhancers(firebaseStore, middleware)
+)
 
 export let persistor = persistStore(store)
