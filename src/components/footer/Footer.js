db8c5022807e02dc5905379a5aef80abfa345cc2
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-02-25 22:04:41 +0600

    refactored Footer to use Toolbar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Typography from 'material-ui/Typography'
+import Toolbar from 'material-ui/Toolbar'
 import { StyledFooter } from './StyledFooter'
 
 export const Footer = () => (
   <StyledFooter>
-    <Typography variant="caption" align="center">
-      © 2018 Play Force Pty Ltd
-    </Typography>
+    <Toolbar className="toolbar">
+      <Typography variant="caption" align="center">
+        © 2018 Play Force Pty Ltd
+      </Typography>
+    </Toolbar>
   </StyledFooter>
 )
--- src/components/footer/StyledFooter.js
+++ src/components/footer/StyledFooter.js
@@ -1,7 +1,16 @@
 import Styled from 'styled-components'
 
 export const StyledFooter = Styled.footer`
-  display: flex;
+  background-color: rgba(51, 51, 51, 0.08);
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
+  span {
+    flex-grow: 1;
+  }
+  /* display: flex;
   align-items: center;
   justify-content: center;
   padding: 10px 0px;
@@ -9,5 +18,5 @@ export const StyledFooter = Styled.footer`
   position: absolute;
   left: 0;
   bottom: 0;
-  width: 100%;
+  width: 100%; */
 `
