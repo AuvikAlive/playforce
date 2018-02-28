cbd20d66d15ccdc2053187d99b93a92b15b3aafa
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-02-28 16:46:37 +0600

    added code splitting for Site

--- src/pages/sites/Sites.js
+++ src/pages/sites/Sites.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Loadable from '../../components/loadable/LoadableLinear'
 import SiteList from './siteList'
-import Site from './site'
+
+const Site = Loadable({
+  loader: () => import('./site'),
+})
+
+Site.preload()
 
 export const Sites = ({
   setNavTitle,
