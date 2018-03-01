22076f25f97749ac84ec2c81d5df77745066897e
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-03-01 16:00:44 +0600

    added directions link on SiteDetail

--- src/pages/sites/site/siteDetail/SiteDetail.js
+++ src/pages/sites/site/siteDetail/SiteDetail.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import IconButton from 'material-ui/IconButton'
 import ArrowBackIcon from 'material-ui-icons/ArrowBack'
 import DeleteIcon from 'material-ui-icons/Delete'
+import EditIcon from 'material-ui-icons/Edit'
+import DirectionsIcon from 'material-ui-icons/Directions'
 import Card, { CardContent } from 'material-ui/Card'
 import List, { ListItem, ListItemText } from 'material-ui/List'
 import Chip from 'material-ui/Chip'
 import Button from 'material-ui/Button'
-import EditIcon from 'material-ui-icons/Edit'
 import { StyledSiteDetail } from './StyledSiteDetail'
 import { StyledNavLink } from '../../../../components/styledNavLink/StyledNavLink'
 import Modal from '../../../../components/modal/Modal'
@@ -31,9 +32,12 @@ export class SiteDetail extends Component {
       match,
       history,
     } = this.props
-
     const id = parseInt(match.params.id, 10) - 1
     const title = data.sites[id].name
+    const site = data.sites[id]
+    const { street, suburb, state, postcode, country } = site
+    const address = `${street}+${suburb}+${state}+${postcode}+${country}`
+    const encodedAddress = encodeURI(address)
 
     setLeftNavComponent(
       <IconButton color="inherit" aria-label="Search" onClick={history.goBack}>
@@ -44,9 +48,24 @@ export class SiteDetail extends Component {
     setNavTitle(title)
 
     setRightNavComponent(
-      <IconButton color="inherit" aria-label="Search" onClick={this.openModal}>
-        <DeleteIcon />
-      </IconButton>,
+      <div>
+        <a
+          target="_blank"
+          href={`https://www.google.com/maps/dir/?api=1&destination=${encodedAddress}`}
+          style={{ color: 'inherit', textDecoration: 'none' }}
+        >
+          <IconButton color="inherit" aria-label="Directions">
+            <DirectionsIcon />
+          </IconButton>
+        </a>
+        <IconButton
+          color="inherit"
+          aria-label="Search"
+          onClick={this.openModal}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </div>,
     )
   }
 
