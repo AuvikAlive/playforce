9e16da651ae25dd72ada5830243692186a1b85f8
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-03-01 18:42:07 +0600

    added AddInspection select content

--- src/pages/sites/site/siteEdit/inspectionsTab/addInspection/AddInspection.js
+++ src/pages/sites/site/siteEdit/inspectionsTab/addInspection/AddInspection.js
@@ -1,8 +1,87 @@
 import React, { Component } from 'react'
+import Card, { CardContent } from 'material-ui/Card'
+import { MenuItem } from 'material-ui/Menu'
+import TextField from 'material-ui/TextField'
+import { StyledAppInspection } from './StyledAddInspection'
+
+const inspectionTypes = ['Routine', 'Operational', 'Comprehensive']
+const frequencies = ['Weekly', 'Monthly', 'Annually']
+const assignees = ['Justin Kaese', 'Joe Bloggs', 'Jane Doe']
 
 export class AddInspection extends Component {
-  state = {}
+  state = {
+    inspectionType: '',
+    frequency: '',
+    assignedTo: '',
+  }
+
+  componentDidMount() {
+    console.log(this.props)
+  }
+
+  onSelectChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
   render() {
-    return <div>Add Inspections</div>
+    const { inspectionType, frequency, assignedTo } = this.state
+
+    return (
+      <StyledAppInspection>
+        <Card className="card">
+          <CardContent>
+            <TextField
+              fullWidth
+              id="inspection-type"
+              select
+              label="Inspection Type"
+              value={inspectionType}
+              onChange={this.onSelectChange('inspectionType')}
+              margin="normal"
+            >
+              {inspectionTypes.map(item => (
+                <MenuItem key={item} value={item}>
+                  {item}
+                </MenuItem>
+              ))}
+            </TextField>
+
+            <TextField
+              fullWidth
+              id="frequency"
+              select
+              label="Frequency"
+              value={frequency}
+              onChange={this.onSelectChange('frequency')}
+              margin="normal"
+            >
+              {frequencies.map(item => (
+                <MenuItem key={item} value={item}>
+                  {item}
+                </MenuItem>
+              ))}
+            </TextField>
+
+            <TextField
+              fullWidth
+              id="assigned-to"
+              select
+              label="Assigned To"
+              value={assignedTo}
+              onChange={this.onSelectChange('assignedTo')}
+              margin="normal"
+            >
+              {assignees.map(item => (
+                <MenuItem key={item} value={item}>
+                  {item}
+                </MenuItem>
+              ))}
+            </TextField>
+          </CardContent>
+        </Card>
+      </StyledAppInspection>
+    )
   }
 }
---
+++ src/pages/sites/site/siteEdit/inspectionsTab/addInspection/StyledAddInspection.js
@@ -0,0 +1,18 @@
+import Styled from 'styled-components'
+import { Content } from '../../../../../../components/content/Content'
+
+export const StyledAppInspection = Styled(Content)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 56*2px - 48px);
+
+  @media (min-width: 600px) {
+    min-height: calc(100vh - 64*2px - 48px);
+  }
+
+  .card {
+    width: 100%;
+    position: relative;
+  }
+`
