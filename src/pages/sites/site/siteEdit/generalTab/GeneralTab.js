235f1d984cd9a6c27f890c65b04902aeede24e6c
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-03-01 13:58:08 +0600

    added all inputs to GeneralTab

--- src/pages/sites/site/siteEdit/generalTab/GeneralTab.js
+++ src/pages/sites/site/siteEdit/generalTab/GeneralTab.js
@@ -10,27 +10,65 @@ const operators = data.operators
 export class GeneralTab extends Component {
   state = {
     operator: { id: '', name: '' },
+    name: '',
+    address: '',
+    division: '',
   }
 
   componentDidMount() {
     const { id } = this.props
-    const operatorId = parseInt(data.sites[id].operator, 10) - 1
-    const operator = {
+    const site = data.sites[id]
+    const {
+      street,
+      suburb,
+      state,
+      postcode,
+      country,
+      operator,
+      division,
+    } = site
+    const operatorId = parseInt(operator, 10) - 1
+
+    const operatorData = {
       id: data.operators[operatorId].id,
       label: data.operators[operatorId].name,
     }
 
-    this.setState({ operator })
+    const name = data.sites[id].name
+    const address = `${street} , ${suburb} ${state} ${postcode}, ${country}`
+
+    this.setState({ operator: operatorData, name, address, division })
   }
 
-  handleChange = event => {
+  onSelectChange = event => {
     const operatorId = parseInt(event.target.value, 10) - 1
 
     this.setState({
       operator: data.operators[operatorId],
     })
   }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
+  // onNameChange = event => {
+  //   this.setState({
+  //     name: event.target.value,
+  //   })
+  // }
+
+  // onAdressChange = event => {
+  //   this.setState({
+  //     address: event.target.value,
+  //   })
+  // }
+
   render() {
+    const { operator, name, address, division } = this.state
+
     return (
       <StyledGeneralTab>
         <Card className="card">
@@ -40,8 +78,8 @@ export class GeneralTab extends Component {
               id="operator"
               select
               label="Operator"
-              value={this.state.operator.id}
-              onChange={this.handleChange}
+              value={operator.id}
+              onChange={this.onSelectChange}
               margin="normal"
             >
               {operators.map(option => (
@@ -50,6 +88,33 @@ export class GeneralTab extends Component {
                 </MenuItem>
               ))}
             </TextField>
+
+            <TextField
+              fullWidth
+              id="name"
+              label="Name"
+              value={name}
+              onChange={this.handleChange('name')}
+              margin="normal"
+            />
+
+            <TextField
+              fullWidth
+              id="address"
+              label="Address"
+              value={address}
+              onChange={this.handleChange('address')}
+              margin="normal"
+            />
+
+            <TextField
+              fullWidth
+              id="division"
+              label="Division"
+              value={division}
+              onChange={this.handleChange('division')}
+              margin="normal"
+            />
           </CardContent>
         </Card>
       </StyledGeneralTab>
