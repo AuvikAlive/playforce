fdee839ad43573ea0e547e0abe6b0b1704ec8dc4
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-03-25 13:35:08 +0600

    removed redundant profile empty check from UserView

--- src/components/sideMenu/userView/UserView.js
+++ src/components/sideMenu/userView/UserView.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Avatar from 'material-ui/Avatar'
 import Typography from 'material-ui/Typography'
-import { isEmpty } from 'react-redux-firebase'
-import { getInitials } from '../../../utilities/getInitials';
+import { getInitials } from '../../../utilities/getInitials'
 import { StyledUserView } from './StyledUserView'
 import backgroundLowWebp from './backgroundLow.webp'
 import backgroundWebp from './background.webp'
@@ -12,37 +11,35 @@ import background from './background.png'
 export const UserView = ({ profile }) => {
   const { image, displayName, email } = profile
   return (
-    !isEmpty(profile) && (
-      <StyledUserView>
-        <div className="content">
-          {image ? (
-            <Avatar alt="User Name" src={image} className="avatar" />
-          ) : (
-            <Avatar className="avatar">{getInitials(displayName)}</Avatar>
-          )}
-          <Typography variant="title" color="inherit">
-            {displayName}
-          </Typography>
-          <Typography variant="subheading" color="inherit">
-            {email}
-          </Typography>
-        </div>
-        <picture className="background">
-          <source srcSet={backgroundLowWebp} type="image/webp" />
-          <source
-            srcSet={backgroundWebp}
-            type="image/webp"
-            media="(min-width: 1080px)"
-          />
-          <source srcSet={backgroundLow} type="image/jpeg" />
-          <source
-            srcSet={background}
-            type="image/jpeg"
-            media="(min-width: 1080px)"
-          />
-          <img src={backgroundLow} alt="background" />
-        </picture>
-      </StyledUserView>
-    )
+    <StyledUserView>
+      <div className="content">
+        {image ? (
+          <Avatar alt="User Name" src={image} className="avatar" />
+        ) : (
+          <Avatar className="avatar">{getInitials(displayName)}</Avatar>
+        )}
+        <Typography variant="title" color="inherit">
+          {displayName}
+        </Typography>
+        <Typography variant="subheading" color="inherit">
+          {email}
+        </Typography>
+      </div>
+      <picture className="background">
+        <source srcSet={backgroundLowWebp} type="image/webp" />
+        <source
+          srcSet={backgroundWebp}
+          type="image/webp"
+          media="(min-width: 1080px)"
+        />
+        <source srcSet={backgroundLow} type="image/jpeg" />
+        <source
+          srcSet={background}
+          type="image/jpeg"
+          media="(min-width: 1080px)"
+        />
+        <img src={backgroundLow} alt="background" />
+      </picture>
+    </StyledUserView>
   )
 }
