22d7004348f3698c8e8b5c30e81969d748d21058
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-04-05 23:10:42 +0600

    decreased portrait image height for compliance issues on add inspection form

--- src/pages/inspections/complianceIssueForm/StyledComplianceIssueForm.js
+++ src/pages/inspections/complianceIssueForm/StyledComplianceIssueForm.js
@@ -6,4 +6,15 @@ import { riskAssessment } from '../../../styledMixins/riskAssessment'
 export const StyledComplianceIssueForm = Styled(Content)`
   ${cardMedia};
   ${riskAssessment};
+  img {
+    height: 50vw;
+    width: auto;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+
+     @media (min-width: 600px) {
+      height: calc(600 * 16 / 9px);
+    }
+  }
 `
--- src/pages/inspections/complianceIssuesList/StyledComplianceIssuesList.js
+++ src/pages/inspections/complianceIssuesList/StyledComplianceIssuesList.js
@@ -2,5 +2,15 @@ import Styled from 'styled-components'
 import { StyledGridList } from '../StyledGridList'
 
 export const StyledComplianceIssuesList = Styled(StyledGridList)`
-  
+  img {
+    height: 50vw;
+    width: auto;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+
+     @media (min-width: 600px) {
+      height: calc(600 * 16 / 9px);
+    }
+  }
 `
