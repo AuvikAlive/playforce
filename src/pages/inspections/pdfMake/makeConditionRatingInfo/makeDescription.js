042cdc853259ab904a529a36d1ad851a551f55d8
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-05-06 17:10:15 +0600

    fixed no margin on makeConditionRatingInfo

--- src/pages/inspections/pdfMake/makeConditionRatingInfo/makeDescription.js
+++ src/pages/inspections/pdfMake/makeConditionRatingInfo/makeDescription.js
@@ -5,6 +5,7 @@ export const makeDescription = () => [
     text:
       '"Play provision should aim at managing the balance between the need to offer risk and the need to keep children safe from serious harm… In play provision exposure to some degree of risk may be of benefit because it satisfies a basic human need and gives children the chance to learn about risk and consequences in a controlled environment." - Foreword to AS 4685.1–2014',
     italics: true,
+    marginBottom: verticalMargin,
   },
   {
     text:
