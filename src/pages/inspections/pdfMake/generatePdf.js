ceed1fa28c5e5b8e6e09914c02ba17c71f197da6
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-06-28 13:46:14 +0600

    removed dynamic imports from generatePdf to check pdf generation speed

--- src/pages/inspections/pdfMake/generatePdf.js
+++ src/pages/inspections/pdfMake/generatePdf.js
@@ -1,26 +1,28 @@
 import { makeDocDefinition } from './makeDocDefinition'
+import vfsFonts from './vfs_fonts'
+import pdfMake from 'pdfmake/build/pdfmake.min'
+
+const { vfs } = vfsFonts.pdfMake
+pdfMake.vfs = vfs
+
+pdfMake.fonts = {
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+    bold: 'Roboto-Medium.ttf',
+    italics: 'Roboto-Italic.ttf',
+    bolditalics: 'Roboto-MediumItalic.ttf',
+  },
+  Oswald: {
+    normal: 'Oswald-Regular.ttf',
+    bold: 'Oswald-Bold.ttf',
+    italics: 'Oswald-Regular.ttf',
+    bolditalics: 'Oswald-Bold.ttf',
+  },
+}
 
 export const generatePdf = async (inspection, certificate) => {
-  const vfsFonts = await import('./vfs_fonts')
-  const pdfMake = await import('pdfmake/build/pdfmake.min')
-
-  const { vfs } = vfsFonts.pdfMake
-  pdfMake.vfs = vfs
-
-  pdfMake.fonts = {
-    Roboto: {
-      normal: 'Roboto-Regular.ttf',
-      bold: 'Roboto-Medium.ttf',
-      italics: 'Roboto-Italic.ttf',
-      bolditalics: 'Roboto-MediumItalic.ttf',
-    },
-    Oswald: {
-      normal: 'Oswald-Regular.ttf',
-      bold: 'Oswald-Bold.ttf',
-      italics: 'Oswald-Regular.ttf',
-      bolditalics: 'Oswald-Bold.ttf',
-    },
-  }
+  // const vfsFonts = await import('./vfs_fonts')
+  // const pdfMake = await import('pdfmake/build/pdfmake.min')
 
   const docDefinition = await makeDocDefinition(inspection, certificate)
 
