f4a9a4132f98e565fd5638ae52782833dd872eb4
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-07-03 14:43:15 +0600

    refactored AddImpactSurface with functions

--- src/pages/inspections/addImpactSurface/AddImpactSurface.js
+++ src/pages/inspections/addImpactSurface/AddImpactSurface.js
@@ -1,59 +1,37 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import IconButton from '@material-ui/core/IconButton'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import { contextTypesTitleLeftNav } from '../../../globals/contextTypesTitleLeftNav'
+import {
+  onComponentDidMountWithTitleLeftNav,
+  onComponentWillUnmountWithTitleLeftNav,
+  showActionGo,
+} from '../../../functions/'
 import { ImpactSurfaceDetailsForm } from '../impactSurfaceDetailsForm/ImpactSurfaceDetailsForm'
+import { submit } from './submit'
+
+const message = 'Test added!'
 
 export class AddImpactSurface extends Component {
   componentDidMount() {
-    const { setNavTitle, setLeftNavComponent } = this.context
-    const { history } = this.props
+    const title = 'Add test'
 
-    setNavTitle('Add test')
-
-    setLeftNavComponent(
-      <IconButton color="inherit" aria-label="go back" onClick={history.goBack}>
-        <ArrowBackIcon />
-      </IconButton>
-    )
+    onComponentDidMountWithTitleLeftNav(this, title)
   }
 
   componentWillUnmount() {
-    const { removeNavTitle, removeLefNavComponent } = this.context
-
-    removeNavTitle()
-    removeLefNavComponent()
-  }
-
-  showActionGoBack = surfaceId => {
-    const { setFeedback, history, inspectionId } = this.props
-
-    setFeedback({ success: 'Test added!' })
-    history.replace(
-      `/inspections/edit/${inspectionId}/impactTest/edit/${surfaceId}`
-    )
-  }
-
-  submit = async data => {
-    const { addSurfaceTest, userId, inspectionId } = this.props
-    const surfaceId = await addSurfaceTest(userId, inspectionId, data)
-
-    return surfaceId
+    onComponentWillUnmountWithTitleLeftNav(this)
   }
 
   render() {
+    const { inspectionId } = this.props
+    const pathHead = `/inspections/edit/${inspectionId}/impactTest/edit/`
+
     return (
       <ImpactSurfaceDetailsForm
-        onSubmit={this.submit}
-        afterSubmit={this.showActionGoBack}
+        onSubmit={submit(this)}
+        afterSubmit={showActionGo(this, message, pathHead)}
       />
     )
   }
 }
 
-AddImpactSurface.contextTypes = {
-  setNavTitle: PropTypes.func,
-  removeNavTitle: PropTypes.func,
-  setLeftNavComponent: PropTypes.func,
-  removeLefNavComponent: PropTypes.func,
-}
+AddImpactSurface.contextTypes = contextTypesTitleLeftNav
