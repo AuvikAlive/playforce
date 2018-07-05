032c90fd1245cbffe4e9197a0cd35f596a546972
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-07-06 04:06:34 +0600

    refactored EditCommonIssue with functions

--- src/pages/settings/editCommonIssue/EditCommonIssue.js
+++ src/pages/settings/editCommonIssue/EditCommonIssue.js
@@ -1,101 +1,33 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import IconButton from '@material-ui/core/IconButton'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
-import DeleteIcon from '@material-ui/icons/Delete'
+import { contextTypesTitleLeftRightNav } from '../../../constants/'
+import {
+  onComponentWillUnmountWithTitleLeftRightNav,
+  showContentWhenLoaded,
+} from '../../../functions/'
 import CommonIssueForm from '../commonIssueForm/'
+import { onComponentDidMount, submit } from './functions/'
 
 export class EditCommonIssue extends Component {
   componentDidMount() {
-    const {
-      setNavTitle,
-      setLeftNavComponent,
-      setRightNavComponent,
-    } = this.context
-    const {
-      history,
-      openDialog,
-      fetchCommonIssue,
-      userId,
-      commonIssue,
-      commonIssueId,
-    } = this.props
-
-    setNavTitle('Edit Issue')
-
-    setLeftNavComponent(
-      <IconButton color="inherit" aria-label="go back" onClick={history.goBack}>
-        <ArrowBackIcon />
-      </IconButton>
-    )
-
-    setRightNavComponent(
-      <IconButton
-        color="inherit"
-        aria-label="delete condition rating"
-        onClick={() => openDialog(this.delete)}
-      >
-        <DeleteIcon />
-      </IconButton>
-    )
-
-    !commonIssue && fetchCommonIssue(userId, commonIssueId)
+    onComponentDidMount(this)
   }
 
   componentWillUnmount() {
-    const {
-      removeNavTitle,
-      removeLefNavComponent,
-      removeRightNavComponent,
-    } = this.context
-
-    removeNavTitle()
-    removeLefNavComponent()
-    removeRightNavComponent()
-  }
-
-  submit = async commonIssue => {
-    const { updateCommonIssue, userId, commonIssueId, setFeedback } = this.props
-
-    await updateCommonIssue(userId, commonIssueId, commonIssue)
-    setFeedback({ success: 'Issue updated!' })
-  }
-
-  showActionGoBack = () => {
-    const { setFeedback, history } = this.props
-
-    setFeedback({ success: 'Issue deleted!' })
-    history.goBack()
-  }
-
-  delete = async () => {
-    const { deleteCommonIssue, userId, commonIssueId } = this.props
-
-    await deleteCommonIssue(userId, commonIssueId)
-    this.showActionGoBack()
+    onComponentWillUnmountWithTitleLeftRightNav(this)
   }
 
   render() {
     const { commonIssue } = this.props
 
-    return commonIssue ? (
+    return showContentWhenLoaded(
+      commonIssue,
       <CommonIssueForm
         buttonText="Update"
         initialData={commonIssue}
-        onSubmit={this.submit}
+        onSubmit={submit(this)}
       />
-    ) : (
-      <LinearProgress />
     )
   }
 }
 
-EditCommonIssue.contextTypes = {
-  setNavTitle: PropTypes.func,
-  removeNavTitle: PropTypes.func,
-  setLeftNavComponent: PropTypes.func,
-  removeLefNavComponent: PropTypes.func,
-  setRightNavComponent: PropTypes.func,
-  removeRightNavComponent: PropTypes.func,
-}
+EditCommonIssue.contextTypes = contextTypesTitleLeftRightNav
---
+++ src/pages/settings/editCommonIssue/functions/deleteCommonIssue.js
@@ -0,0 +1,11 @@
+import { showActionGoBack } from '../../../../functions/'
+
+export const deleteCommonIssue = component => async () => {
+  const { deleteCommonIssue, userId, commonIssueId } = this.props
+
+  await deleteCommonIssue(userId, commonIssueId)
+
+  const message = 'Issue deleted!'
+
+  showActionGoBack(component, message)
+}
