fc821a86cb3cd6844ee64f1c760ffd0de1c0eff8
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-08-02 12:32:30 +0600

    fixed drop test not deleting on EditDropTest

--- src/pages/inspections/editDropTest/functions/onComponentDidMount.js
+++ src/pages/inspections/editDropTest/functions/onComponentDidMount.js
@@ -2,6 +2,7 @@ import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { onComponentDidMountWithTitleLeftNav } from '../../../../functions/'
+import { deleteDropTest } from './deleteDropTest';
 
 export const onComponentDidMount = component => {
   const { setRightNavComponent } = component.context
@@ -15,7 +16,7 @@ export const onComponentDidMount = component => {
     <IconButton
       color="inherit"
       aria-label="delete surface test"
-      onClick={() => openDialog(component.delete)}
+      onClick={() => openDialog(deleteDropTest(component))}
     >
       <DeleteIcon />
     </IconButton>
