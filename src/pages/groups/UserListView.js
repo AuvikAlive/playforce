dc983a16921e5ea30bc375c428b7cf503e0ef72e
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-08-12 20:19:27 +0600

    added EmptyListPlaceholder to group components

--- src/pages/groups/UserListView.js
+++ src/pages/groups/UserListView.js
@@ -6,7 +6,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import IconButton from '@material-ui/core/IconButton'
 import CheckIcon from '@material-ui/icons/Check'
-import Typography from '@material-ui/core/Typography'
+import { EmptyListPlaceholder } from '../../components/emptyListPlacehoder/EmptyListPlaceholder'
 
 export const UserListView = ({
   users,
@@ -46,8 +46,6 @@ export const UserListView = ({
       </List>
     </Paper>
   ) : (
-    <Typography variant="title" align="center">
-      Try adding a member to get started!
-    </Typography>
+    <EmptyListPlaceholder text="Try adding a member to get started!" />
   )
 }
