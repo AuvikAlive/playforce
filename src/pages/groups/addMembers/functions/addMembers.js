ab066e8bb23ae7e09af51c7b08784601e44f4e7e
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-09-13 21:45:16 +0600

    fixed AddMembers select mode nav toggle

--- src/pages/groups/addMembers/functions/addMembers.js
+++ src/pages/groups/addMembers/functions/addMembers.js
@@ -1,10 +1,12 @@
+import { setSelectMode } from './setSelectMode'
+
 export const addMembers = component => async () => {
   const { selectedItems } = component.state
   const { addMembers, id } = component.props
 
   try {
     await addMembers(id, selectedItems)
-    component.setSelectMode(false)
+    setSelectMode(component, 'Add Members')(false)
   } catch (error) {
     console.log(error)
   }
