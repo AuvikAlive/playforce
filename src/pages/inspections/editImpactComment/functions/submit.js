a312b8afe7e4a722b23d81aa6b5900e1fda063bb
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2018-10-08 21:27:45 +0600

    changed EditImpactComment to be able to submit empty comment

--- src/pages/inspections/editImpactComment/functions/submit.js
+++ src/pages/inspections/editImpactComment/functions/submit.js
@@ -5,17 +5,24 @@ export const submit = component => async () => {
 
   setFeedback({ error: '', loading: true })
 
-  if (comment) {
-    try {
-      await saveImpactComment(comment)
-      setFeedback({ success: 'Comment saved!', loading: false })
-    } catch (error) {
-      setFeedback({ error: error.message, loading: false })
-    }
-  } else {
-    setFeedback({
-      error: 'Please fill up the form correctly!',
-      loading: false,
-    })
+  try {
+    await saveImpactComment(comment)
+    setFeedback({ success: 'Comment saved!', loading: false })
+  } catch (error) {
+    setFeedback({ error: error.message, loading: false })
   }
+
+  // if (comment) {
+  //   try {
+  //     await saveImpactComment(comment)
+  //     setFeedback({ success: 'Comment saved!', loading: false })
+  //   } catch (error) {
+  //     setFeedback({ error: error.message, loading: false })
+  //   }
+  // } else {
+  //   setFeedback({
+  //     error: 'Please fill up the form correctly!',
+  //     loading: false,
+  //   })
+  // }
 }
