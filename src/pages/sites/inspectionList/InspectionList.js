9da1138fcbdee20f9e1a5df98fcdaa9fda405cb6
Mir Neamul Karim <auvik1@yahoo.com>
2018-10-31 23:09:40 +0600

    fixed location undefined error on InspectionList on SiteEdit

--- src/pages/sites/inspectionList/InspectionList.js
+++ src/pages/sites/inspectionList/InspectionList.js
@@ -13,6 +13,7 @@ import { ListAvatar } from '../../../components/listAvatar/ListAvatar'
 import {
   contextTypesTitle,
   contextTypesUnsubscriber,
+  inspectionTypes,
 } from '../../../constants/'
 import { showContentWhenLoaded } from '../../../functions/'
 import { StyledInspectionList } from './StyledInspectionList'
@@ -41,12 +42,23 @@ export class InspectionList extends Component {
           <Paper className="paper">
             <List component="nav" disablePadding>
               {inspectionsBySite.map(
-                ({ id, inspectionNumber, cover }, index, list) => {
-                  const { location, client } = cover
+                (
+                  { id, inspectionNumber, cover, impactGeneralInfo },
+                  index,
+                  list
+                ) => {
+                  const { location, client, inspectionType } =
+                    cover || impactGeneralInfo
                   const { name, suburb } = location
 
+                  const inspectionTypeUrl =
+                    inspectionType.value || inspectionTypes[0].value
+
                   return (
-                    <StyledNavLink to={`/inspections/edit/${id}`} key={id}>
+                    <StyledNavLink
+                      to={`/inspections/${inspectionTypeUrl}/edit/${id}`}
+                      key={id}
+                    >
                       <ListItem divider button>
                         <ListAvatar text={inspectionNumber} />
                         <ListItemText
